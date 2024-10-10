c168740cb00f003872b16ccf08065844e8d27dcb
Esharaqaj <143230329+Esharaqaj@users.noreply.github.com>
2024-10-10 12:35:26 -0400

    Update hw05.h

--- hw05.h
+++ hw05.h
@@ -1,22 +1,53 @@
-#include <stdio.h>
-#include <stdbool.h>
+#ifndef __HW05_H__
+#define __HW05_H__
 #include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+#include <string.h>
 
-typedef struct point {
-    int x, y;
-} Point;
+typedef struct Node {
+    int x;
+    int y;
+    struct Node* left;
+    struct Node* right;
+    int copy;
+} Node;
 
-typedef struct tNode {
-    Point point;
-    struct tNode *left;
-    struct tNode *right;
-    int height;
-} TNode;
+typedef struct List {
+    int x;
+    int y;
+    int copy;
+    struct List* next;
+} List;
 
-// Function prototypes
-int height(TNode* node);
-int getBalance(TNode* node);
-int max(int a, int b);
-int isInCircle(Point p, int h, int k, int r);
-void search(TNode* node, int h, int k, int r, int* count);
-void freeTree(TNode* node);
+void printList(List*);
+List* createLinkNode(int, int); 
+List* linkInsertion(List* root, int, int);
+List* splitList(List*);
+List* mergeList(List*, List*);
+List* MergeSort(List*);
+Node* listToBst(List*);                   // Convert sorted linked list to a balanced BST
+Node* createBstNode(int ,int, int);   // Create a new BST node
+void inOrderTraversal(Node*);             // Perform in-order traversal of the BST
+int bstSearch(Node*, int, int, int);
+bool yChecker(Node*, int,int, int);
+void deallocate(Node*);
+List* getMiddleNode(List*);
+
+
+
+// Node* insert_node(Node*, int);
+// int getBalance(Node*);
+// int height(Node*);
+// int max(int, int);
+// Node* rightRotate(Node*);
+// Node* leftRotate(Node*);
+// int getDistance(int, int, int, int);
+// Node* createNode(int);
+// void inorderTraversal(Node*);
+// int avlSearch(Node*, int);
+// void deallocate(Node*);
+
+
+#endif
